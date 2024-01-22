bff4a169273934fc4cc299a7f351fe3633a99de7
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-01-22 18:07:07 +0000

    Sec 5.39

---
+++ s5_39/barbershop.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"time"
+
+	"github.com/fatih/color"
+)
+
+type BarberShop struct {
+	ShopCapacity    int
+	HaircutDuration time.Duration
+	NumberOfBarbers int
+	BarbersDoneChan chan bool
+	ClientsChan     chan string
+	Open            bool
+}
+
+func (shop *BarberShop) addBarber(barber string) {
+	shop.NumberOfBarbers++
+
+	go func() {
+		isSleeping := false
+
+		color.Yellow("%s goes to the waiting room to check for clients", barber)
+		for {
+			// if there are no clients, the barber goes to sleep
+			if len(shop.ClientsChan) == 0 {
+				color.Yellow("There is nothing to do, so %s takes a nap", barber)
+			}
+
+			client, shopOpen := <-shop.ClientsChan
+			if shopOpen {
+				if isSleeping {
+					color.Yellow("%s wakes %s up", client, barber)
+					isSleeping = false
+				}
+
+				// cut hair
+				shop.cutHair(barber, client)
+			} else {
+				// shop is closed, so send the barber home and close this goroutine
+				shop.sendBarberHome(barber)
+			}
+		}
+	}()
+}
+
+func (shop *BarberShop) cutHair(barber string, client string) {
+	color.Green("%s is cutting %s's hair.", barber, client)
+	time.Sleep(shop.HaircutDuration)
+	color.Green("%s is finished cutting %s's hair.", barber, client)
+}
+
+func (shop *BarberShop) sendBarberHome(barber string) {
+	color.Cyan("%s is going home", barber)
+	shop.BarbersDoneChan <- true
+}
+
+func (shop *BarberShop) closeShopForDay() {
+	color.Cyan("Closing shop for the day.")
+	close(shop.ClientsChan)
+	shop.Open = false
+
+	for a := 1; a <= shop.NumberOfBarbers; a++ {
+		<-shop.BarbersDoneChan
+	}
+
+	close(shop.BarbersDoneChan)
+
+	color.Green("--------------------------------------------------------------------")
+	color.Green("The barbershop is now closed for the day, and everyone has gone home.")
+}
