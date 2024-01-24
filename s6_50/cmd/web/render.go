8b62d3487ca6fd176e06fe814a35ab9cb7268efa
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-01-24 18:54:33 +0000

    Sec 6.50

---
+++ s6_50/cmd/web/render.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+	"time"
+)
+
+var pathToTemplates = "./cmd/web.templates"
+
+type TemplateData struct {
+	StringMap     map[string]string
+	IntMap        map[string]int
+	FloatMap      map[string]float64
+	Data          map[string]any
+	Flash         string
+	Warning       string
+	Error         string
+	Authenticated bool
+	Now           time.Time
+	// User          *data.User
+}
+
+func (app *Config) render(w http.ResponseWriter, r *http.Request, t string, td *TemplateData) {
+	partials := []string{
+		fmt.Sprintf("%s/base.layout.gohtml", pathToTemplates),
+		fmt.Sprintf("%s/header.partial.gohtml", pathToTemplates),
+		fmt.Sprintf("%s/navbar.partial.gohtml", pathToTemplates),
+		fmt.Sprintf("%s/footer.partial.gohtml", pathToTemplates),
+		fmt.Sprintf("%s/alerts.partial.gohtml", pathToTemplates),
+	}
+
+	var templateSlice []string
+	templateSlice = append(templateSlice, fmt.Sprintf("%s/%s", pathToTemplates, t))
+
+	templateSlice = append(templateSlice, partials...)
+
+	if td == nil {
+		td = &TemplateData{}
+	}
+
+	templ, err := template.ParseFiles(templateSlice...)
+	if err != nil {
+		app.ErrorLog.Printf("%s\n", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = templ.Execute(w, app.AddDefaultData(td, r))
+	if err != nil {
+		app.ErrorLog.Printf("%s\n", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (app *Config) AddDefaultData(td *TemplateData, r *http.Request) *TemplateData {
+	td.Flash = app.Session.PopString(r.Context(), "flash")
+	td.Warning = app.Session.PopString(r.Context(), "warning")
+	td.Error = app.Session.PopString(r.Context(), "error")
+	if app.IsAuthenticated(r) {
+		td.Authenticated = true
+		// TODO - get more user information
+	}
+	td.Now = time.Now()
+
+	return td
+}
+
+func (app *Config) IsAuthenticated(r *http.Request) bool {
+	return app.Session.Exists(r.Context(), "userID")
+}
