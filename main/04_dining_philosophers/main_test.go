407016a1b065d24ff06e4caf15e9a8b1edb14a31
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-01-25 19:45:26 +0000

    Sec 4.31

---
+++ main/04_dining_philosophers/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestMain(t *testing.T) {
+	eatTime = 0 * time.Second
+	sleepTime = 0 * time.Second
+	thinkTime = 0 * time.Second
+
+	for i := 0; i < 10; i++ {
+		orderFinished = []string{}
+		dine()
+		if len(orderFinished) != 5 {
+			t.Errorf("incorrect len of order finished; expected 5, got %d", len(orderFinished))
+		}
+	}
+}
+
+func TestDineWithVaryingDelays(t *testing.T) {
+	var theTests = []struct {
+		name  string
+		delay time.Duration
+	}{
+		{"zero delay", time.Second * 0},
+		{"quartr second delay", time.Millisecond * 250},
+		{"half second delay", time.Millisecond * 500},
+	}
+
+	for _, e := range theTests {
+		orderFinished = []string{}
+
+		eatTime = e.delay
+		sleepTime = e.delay
+		thinkTime = e.delay
+
+		dine()
+		if len(orderFinished) != 5 {
+			t.Errorf("%s: incorrect len of order finished; expected 5, got %d",
+				e.name, len(orderFinished))
+		}
+
+	}
+}
