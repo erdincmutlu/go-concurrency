d6a6c6a5eab35035a85e430d7c5bb3c998155ad2
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-01-25 19:46:40 +0000

    Sec 5.33

---
+++ main/05_simple_channels/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func shout(ping <-chan string, pong chan<- string) {
+	for {
+		s, ok := <-ping
+		if !ok {
+			// not ok means channel lwas closed and zero value was returned
+			// do something
+		}
+		pong <- fmt.Sprintf("%s!!!", strings.ToUpper(s))
+	}
+}
+func main() {
+	// create two channels
+	ping := make(chan string)
+	pong := make(chan string)
+
+	go shout(ping, pong)
+
+	fmt.Printf("Type something and press ENTER (enter Q to quit)")
+
+	for {
+		// print a prompt
+		fmt.Printf("-> ")
+
+		// get user input
+		var userInput string
+		_, _ = fmt.Scanln(&userInput)
+		if strings.ToLower(userInput) == "q" {
+			break
+		}
+
+		ping <- userInput
+
+		// wait for a response
+		response := <-pong
+		fmt.Printf("Response: %s\n", response)
+
+	}
+
+	fmt.Printf("All done. Closing channels.")
+	close(ping)
+	close(pong)
+}
