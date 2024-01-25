80fed9bb9f8a66efc8504edbc6f25694fa0bab30
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-01-25 19:35:30 +0000

    Sec 3.19

--- main/02_example2_02/main_test.go
+++ main/02_example2_02/main_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	stdOut := os.Stdout
+
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	main()
+	_ = w.Close()
+
+	result, _ := io.ReadAll(r)
+	output := string(result)
+
+	os.Stdout = stdOut
+
+	if !strings.Contains(output, "$34320.00") {
+		t.Errorf("Wrong balance returned")
+	}
+}
